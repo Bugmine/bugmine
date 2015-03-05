4a9d309603fcf0d8701ebefe64e36f924a9a5a73
Stefan Schmid <stefanschmid35@googlemail.com>
2015-03-05 21:30:50 +0100

    Moved login form strings to language file Added required attribute for email and password in the login form

---
+++ resources/lang/en/forms.php
@@ -0,0 +1,18 @@
+<?php
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Login form Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used in the login form.
+	|
+	*/
+	'title_sign_in' => "Sign in",
+	'email' => "Email Address",
+	'password' => "Password",
+	'remember' => "Remember me",
+	'sign_in' => "Sign in",
+	'forgot_your_password' => "Forgot your password?",
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="col-md-4 col-md-offset-4">
     <div class="panel panel-default">
-    <div class="panel-heading">Login</div>
+    <div class="panel-heading">{{ trans('forms.title_sign_in') }}</div>
     <div class="panel-body">
         @if($errors->any())
             <div class="alert alert-danger" role="alert">
@@ -18,23 +18,23 @@
         {!! Form::hidden('_token', csrf_token()) !!}
         <!--- Username Field --->
         <div class="form-group">
-            {!! Form::label('email', 'Email Address:') !!}
-            {!! Form::email('email', null, ['class' => 'form-control']) !!}
+            {!! Form::label('email', trans('forms.email')) !!}
+            {!! Form::email('email', null, ['class' => 'form-control', 'required' => 'required']) !!}
         </div>
         <!--- Password Field --->
         <div class="form-group">
-            {!! Form::label('password', 'Password:') !!}
-            {!! Form::password('password', ['class' => 'form-control']) !!}
+            {!! Form::label('password', trans('forms.password')) !!}
+            {!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
         </div>
         <!--- Remember Checkbox --->
         <div class="form-group">
             {!! Form::checkbox('remember','value', false) !!}
-            {!! Form::label('remember', 'Remember me') !!}
+            {!! Form::label('remember', trans('forms.remember')) !!}
         </div>
         <!--- Submit Field --->
         <div class="form-group">
-            {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
-            <a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+            {!! Form::submit(trans('forms.sign_in'), ['class' => 'btn btn-primary']) !!}
+            <a class="btn btn-link" href="{{ url('/password/email') }}">{{ trans('forms.forgot_your_password') }}</a>
         </div>
         {!! Form::close() !!}
     </div>
